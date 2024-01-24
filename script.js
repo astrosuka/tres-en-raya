d7e3f02d3bbb3ce0eba62de2ddda2f6bfe45c8d3
astrosuka <yegresvoselok@gmail.com>
2024-01-24 08:59:29 -0300

    Add createPlayer

--- script.js
+++ script.js
@@ -2,14 +2,28 @@ const gameboard = (function () {
     let arr = [];
     let rows = 3;
     let columns = 3;
-
-    for (let i = 0; i < rows; i++) {
-        arr[i] = [];
-        for (let j = 0; j < columns; j++) {
-            arr[i][j] = j;
+    
+    const clearBoard = () => {
+        for (let i = 0; i < rows; i++) {
+            arr[i] = [];
+            for (let j = 0; j < columns; j++) {
+                arr[i][j] = 0;
+            }
         }
-    }
-
+    };
+    
+    clearBoard();
     const getBoard = () => arr;
-    return { getBoard };
+    return { getBoard, clearBoard };
 })();
+
+function createPlayer (name, symbol) {
+    const playerName = name;
+    let score = 0;
+
+    const getScore = () => score;
+    const increaseScore = () => score++;
+    const getSymbol = () => symbol;
+
+    return{ playerName, getScore, increaseScore, getSymbol };
+};
